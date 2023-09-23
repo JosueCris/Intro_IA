b7e281350e03a6d51409c5a6c08d47becc2a7b46
Josue Téllez <josuetllezh@gmail.com>
2023-09-23 17:07:07 -0600

    Mejoras de metodos y agregado de las rotaciones de equilibrio

--- IIA/Arboles/Tree.py
+++ IIA/Arboles/Tree.py
@@ -5,6 +5,7 @@ class Tree(object):
         self.left = None
         self.right = None
 
+
     def add(self, data):
         if data < self.data:
             if self.left:
@@ -16,6 +17,9 @@ class Tree(object):
                 self.right.add(data)
             else:
                 self.right = Tree(data)
+        # return self.balance_tree()
+
+
 
     def pre_orden(self):
         print(self.data, "-> ", end="")
@@ -24,6 +28,7 @@ class Tree(object):
         if self.right:
             self.right.pre_orden()
 
+
     def in_orden(self):
         if self.left:
             self.left.in_orden()
@@ -31,12 +36,14 @@ class Tree(object):
         if self.right:
             self.right.in_orden()
 
+
     def post_orden(self):
         if self.left:
             self.left.post_orden()
         if self.right:
             self.right.post_orden()
         print(self.data, "-> ", end="")
+
 
     def search(self, data):
         if not self:
@@ -50,34 +57,50 @@ class Tree(object):
         if data > self.data:
             if not self.right:
                 return None
-        return self.right.search(data)
+            return self.right.search(data)
     
-    def more_to_right(self):
+    
+    def rightmost(self):
         if not self:
             return None
         if not self.right:
             return self
-        return self.right.more_to_right()
+        return self.right.rightmost()
 
-    def more_to_left(self):
+
+    def leftmost(self):
         if not self:
             return None
         if not self.left:
             return self
-        return self.left.more_to_left()
+        return self.left.leftmost()
 
-    # def delete(self, data):
-    #     if not self:
-    #         return None
-    #     item = self.search(data)
-    #     if data is item.data:
-    #         aux = self
-            
-    #     if data < item.data:
-    #         self.left.delete(data)
-    #     if data > item.data:
-    #         self.right.delete(data)
-       
+
+    def delete(self, data):
+        if not self:
+            return None
+        # Buscamos el nodo a eliminar
+        if data < self.data:
+            if self.left:
+                self.left = self.left.delete(data)
+        elif data > self.data:
+            if self.right:
+                self.right = self.right.delete(data)
+        else:
+            # Si lo encontramos, procedemos a eliminarlo
+            if not self.left:
+                return self.right
+            elif not self.right:
+                return self.left
+            else:
+                # Si el nodo tiene dos hijos, encontrar el sucesor inmediato
+                successor = self.right.leftmost()
+                self.data = successor.data
+                # Eliminar el sucesor inmediato
+                self.right = self.right.delete(successor.data)
+        return self  # Importante: devolver el nodo actual después de cualquier modificación
+
+
     def nodes_cant(self):
         nodes = 1
         if self is None:
@@ -88,6 +111,7 @@ class Tree(object):
             nodes += self.right.nodes_cant()
         return nodes
 
+
     def leafs_cant(self): 
         if self is None:
             return 0
@@ -95,11 +119,13 @@ class Tree(object):
             return 1
         return self.left.leafs_cant() + self.right.leafs_cant()
 
+
 ##  Igual se puede usar directo con la funcion max de python pero hice mi propia implementacion del retorno
     def max(self, left, right):
         if left > right:
             return left
         return right
+
 
     def height(self):
         left = 1
@@ -112,51 +138,94 @@ class Tree(object):
             right += self.right.height()
         return self.max(left, right)
 
+
 ##  Inicio de la pesadilla :'v
     def balance_factor(self):
         if not self:
             return 0
-        return self.right.height() - self.left.height()
+        left_height = self.left.height() if self.left else 0
+        right_height = self.right.height() if self.right else 0
+        return right_height - left_height
 
-    def lsr(self):
-        if not self:
-            return 0
-        factor = self.balance_factor()
-        if factor < 0:
-            aux = self.right
-            self.right = self
-            self = aux
-        return 1
 
-    def rsr(self):
+    def slr(self):
         if not self:
-            return 0
-        factor = self.balance_factor()
-        if factor > 0:
-            if self.right:
-                aux = self.left
-                aux.left = self
-                self = aux
-        return 1
+            return None
+        if not self.right:
+            return None
+        aux = self.right
+        self.right = aux.left
+        aux.left = self
+        return aux
+
+    
+    def srr(self):
+        if not self:
+            return None
+        if not self.left:
+            return None
+        aux = self.left
+        self.left = aux.right
+        aux.right = self
+        return aux
+    
         
-    def lcr(self):
+    def clr(self):
         if not self:
-            return 0
-        aux1 = self.left
-        aux2 = aux1.right
-        aux1.right = aux2.left
-        self.left = aux2.right
-        aux2.right = self
-        self = aux2
-        return 1
+            return None
+        if not self.right:
+            return None
+        aux1 = self.right
+        aux2 = aux1.left
+        aux1.left = aux2.right
+        aux2.right = aux1
+        self.right = aux1.left
+        aux2.left = self
+        return aux2
 
-    def rcr(self):
+
+    def crr(self):
         if not self:
-            return 0
+            return None
+        if not self.left:
+            return None
         aux1 = self.left
         aux2 = aux1.right
         aux1.right = aux2.left
-        self.left = aux2.right
+        aux2.left = aux1
+        self.left = aux1.right
         aux2.right = self
-        self = aux2
-        return 1
+        return aux2
+    
+    
+    def balance_left(self):
+        if not self:
+            return None
+        if not self.left:
+            return None
+        if not self.left.right:
+            return self.srr()
+        else:
+            return self.crr()
+
+
+    def balance_right(self):
+        if not self:
+            return None
+        if not self.right:
+            return None
+        if not self.right.left:
+            return self.slr()
+        else:
+            return self.clr()
+
+
+    def balance_tree(self):
+        if not self:
+            return None
+        result = self.balance_factor()
+        if result < -1:
+            return self.balance_left()
+        if result > 1:
+            return self.balance_right()
+        return self
--- IIA/Arboles/Tree_Test.py
+++ IIA/Arboles/Tree_Test.py
@@ -1,117 +1,125 @@
-from pty import slave_open
 from Tree import Tree
 
 ##  MAIN
 # raiz = Tree(55)
 # raiz.add(27)
-# raiz.add(32)
-# raiz.add(64)
+# # raiz.add(32)
+# # raiz.add(64)
+# raiz.add(57)
 # raiz.add(59)
 # raiz.add(77)
-raiz = Tree(50)
-raiz.add(25)
-raiz.add(10)
+# # raiz = Tree(50)
+# # raiz.add(25)
+# # raiz.add(10)
+# # raiz.add(70)
+# # raiz.add(8)
 
 
-##  IMPRESIONES
-print("PRE-ORDEN")
-raiz.pre_orden()
-print("\n")
+# ##  IMPRESIONES
+# print("PRE-ORDEN")
+# raiz.pre_orden()
+# print("\n")
 
-print("IN-ORDEN")
-raiz.in_orden()
-print("\n")
+# print("IN-ORDEN")
+# raiz.in_orden()
+# print("\n")
 
-print("POST-ORDEN")
-raiz.post_orden()
-print("\n")
+# print("POST-ORDEN")
+# raiz.post_orden()
+# print("\n")
 
 
-##  BUSQUEDAS
-nodo = raiz.search(55)
-if not nodo:
-    print("No existe")
-else:
-    print(f"Encontrado: {nodo.data}")
+# ##  BUSQUEDAS
+# nodo = raiz.search(55)
+# if not nodo:
+#     print("No existe")
+# else:
+#     print(f"Encontrado: {nodo.data}")
     
-nodo = raiz.search(24)
-if not nodo:
-    print("No existe")
-else:
-    print(f"Encontrado: {nodo.data}")
+# nodo = raiz.search(24)
+# if not nodo:
+#     print("No existe")
+# else:
+#     print(f"Encontrado: {nodo.data}")
 
 
-##  CALCULO DE VALORES
-nodos = raiz.nodes_cant()
-hojas = raiz.leafs_cant()
-altura = raiz.height()
-factor = raiz.balance_factor()
-print(f"Cantidad de nodos: {nodos}")
-print(f"Cantidad de hojas: {hojas}")
-print(f"Altura del arbol: {altura}")
-print(f"Factor de equilibrio: {factor}")
+# ##  CALCULO DE VALORES
+# nodos = raiz.nodes_cant()
+# hojas = raiz.leafs_cant()
+# altura = raiz.height()
+# factor = raiz.balance_factor()
+# print(f"Cantidad de nodos: {nodos}")
+# print(f"Cantidad de hojas: {hojas}")
+# print(f"Altura del arbol: {altura}")
+# print(f"Factor de equilibrio: {factor}")
 
+# print(f"El mas a la derecha del lado izquierdo: {raiz.left.rightmost().data}")
+# print(f"El mas a la derecha del lado derecho: {raiz.right.rightmost().data}")
+# print(f"El mas a la izquierda del lado izquierdo: {raiz.left.leftmost().data}")
+# print(f"El mas a la izquierda del lado derecho: {raiz.right.leftmost().data}")
 
-print(f"El mas a la derecha del lado izquierdo: {raiz.left.more_to_right().data}")
-print(f"El mas a la derecha del lado derecho: {raiz.right.more_to_right().data}")
-print(f"El mas a la izquierda del lado izquierdo: {raiz.left.more_to_left().data}")
-print(f"El mas a la izquierda del lado derecho: {raiz.right.more_to_left().data}")
+# # raiz = raiz.balance_tree()
 
-raiz.lsr()
+# ##  CALCULO DE VALORES DESPUÉS DE LA ROTACIÓN
+# nodos = raiz.nodes_cant()
+# hojas = raiz.leafs_cant()
+# altura = raiz.height()
+# factor = raiz.balance_factor()
+# print(f"\nCantidad de nodos: {nodos}")
+# print(f"Cantidad de hojas: {hojas}")
+# print(f"Altura del arbol: {altura}")
+# print(f"Factor de equilibrio: {factor}")
 
-print(f"\nCantidad de nodos: {nodos}")
-print(f"Cantidad de hojas: {hojas}")
-print(f"Altura del arbol: {altura}")
-print(f"Factor de equilibrio: {factor}")
+# print(f"El mas a la derecha del lado izquierdo: {raiz.left.rightmost().data}")
+# print(f"El mas a la derecha del lado derecho: {raiz.right.rightmost().data}")
+# print(f"El mas a la izquierda del lado izquierdo: {raiz.left.leftmost().data}")
+# print(f"El mas a la izquierda del lado derecho: {raiz.right.leftmost().data}")
+
 
 
 ## MENU INTERACTIVO PARA EL USUARIO QUE DESEE MANEJAR EL ARBOL
-# raiz = None
-# op = 9
+raiz = None
+op = 9
 
-# while op != 0:
-#     print("Selecciona una opcion para el arbol binario de busqueda:")
-#     op = int(input("\t[1]: Insertar\n\t[2]: Pre-Orden\n\t[3]: In-Orden\n\t[4]: Post-Orden\n\t[5]: Buscar\n\t[6]: Eliminar\n\t[7]: Cant. Nodos\n\t[8]: Cant. Hojas\n\t[9]: Altura\n\t[10]: Fact. Equilibrio\n\t[0]: Salir\n> "))
+while op != 0:
+    print("Selecciona una opcion para el arbol binario de busqueda - AVL:")
+    op = int(input("\t[1]: Insertar\n\t[2]: Pre-Orden\n\t[3]: In-Orden\n\t[4]: Post-Orden\n\t[5]: Buscar\n\t[6]: Eliminar\n\t[7]: Datos\n\t[0]: Salir\n> "))
 
-#     if op == 1:
-#         num = int(input("Ingresa un numero en el arbol: "))
-#         if not raiz:
-#             raiz = Tree(num)
-#         else:
-#             raiz.add(num)
-#     if op == 2:
-#         print("PRE-ORDEN")
-#         raiz.pre_orden()
-#         print("\n")
-#     if op == 3:
-#         print("IN-ORDEN")
-#         raiz.in_orden()
-#         print("\n")
-#     if op == 4:
-#         print("POST-ORDEN")
-#         raiz.post_orden()
-#         print("\n")
-#     if op == 5:
-#         num = int(input("Ingresa el numero a buscar: "))
-#         nodo = raiz.search(num)
-#         if not nodo:
-#             print("No existe \n")
-#         else:
-#             print(f"Encontrado: {nodo.data} \n")
-#     if op == 6:
-#         num = int(input("Ingresa el numero a eliminar: "))
-#         raiz.delete(num)
-#     if op == 7:
-#         nodos = raiz.nodes_cant()
-#         print(f"Cantidad de nodos: {nodos} \n")
-#     if op == 8:
-#         hojas = raiz.leafs_cant()
-#         print(f"Cantidad de hojas: {hojas} \n")
-#     if op == 9:
-#         altura = raiz.height()
-#         print(f"Altura del arbol: {altura} \n")
-#     if op == 10:
-#         factor = raiz.balance_factor
-#         print(f"Factor de equilibrio: {factor} \n")
-
-# print("Vuelva pronto :)")
+    if op == 1:
+        num = int(input("Ingresa un numero en el arbol: "))
+        if not raiz:
+            raiz = Tree(num)
+        else:
+            raiz.add(num)
+    if op == 2:
+        print("PRE-ORDEN")
+        raiz.pre_orden()
+        print("\n")
+    if op == 3:
+        print("IN-ORDEN")
+        raiz.in_orden()
+        print("\n")
+    if op == 4:
+        print("POST-ORDEN")
+        raiz.post_orden()
+        print("\n")
+    if op == 5:
+        num = int(input("Ingresa el numero a buscar: "))
+        nodo = raiz.search(num)
+        if not nodo:
+            print("No existe \n")
+        else:
+            print(f"Encontrado: {nodo.data} \n")
+    if op == 6:
+        num = int(input("Ingresa el numero a eliminar: "))
+        raiz = raiz.delete(num)
+    if op == 7:
+        nodos = raiz.nodes_cant()
+        hojas = raiz.leafs_cant()
+        altura = raiz.height()
+        factor = raiz.balance_factor()
+        print(f"Cantidad de nodos: {nodos}")
+        print(f"Cantidad de hojas: {hojas}")
+        print(f"Altura del arbol: {altura}")
+        print(f"Factor de equilibrio: {factor}")
+print("Vuelva pronto :)")
